2632efd71a0661b6cbab261fbb335df8c499859b
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-10-07 13:40:33 +0900

    [add] style

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -7,17 +7,32 @@ const Container = styled.div`
   height: 5vh;
   background-color: #81c784;
   width: 100%;
-  color: rgba(255, 255, 255, 0.4);
+
   font-weight: bold;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
+const CustomLink = styled.a`
+  text-decoration: none;
+  color: rgba(255, 255, 255, 0.4);
+  transition: color 0.5s;
+  &:hover {
+    color: rgba(255, 255, 255, 0.7);
+  }
+`;
+
 const Footer: React.FC = () => {
   return (
     <Container>
-      <p>© MokeeeMokeee</p>
+      <CustomLink
+        rel='noopener noreferrer'
+        target='_blank'
+        href='https://github.com/MokeeeMokeee/MountingGorilla'
+      >
+        <p>© MokeeeMokeee</p>
+      </CustomLink>
     </Container>
   );
 };
